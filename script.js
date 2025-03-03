faa0f277a2e2259299a35eb17916d94aa72db884
falker <maurizio.falconi47@gmail.com>
2025-03-04 00:28:00 +0100

    v1

--- script.js
+++ script.js
@@ -1,9 +1,22 @@
 // Scroll smooth per i link interni
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-    anchor.addEventListener('click', function(e) {
+  anchor.addEventListener('click', function(e) {
       e.preventDefault();
-      document.querySelector(this.getAttribute('href')).scrollIntoView({
-        behavior: 'smooth'
+      const targetElement = document.querySelector(this.getAttribute('href'));
+      const offset = 100; // Modifica questo valore per regolare l'offset desiderato
+      const elementPosition = targetElement.getBoundingClientRect().top;
+      const offsetPosition = elementPosition + window.pageYOffset - offset;
+      window.scrollTo({
+          top: offsetPosition,
+          behavior: "smooth"
       });
-    });
   });
+});
+
+
+  document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById("currentYear").textContent = new Date().getFullYear();
+  });
+
+
+  document.getElementById("currentYear").textContent = new Date().getFullYear();
