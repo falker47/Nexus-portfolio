4a6fbe365c81e9809a9df1c9ff0e9cec31b12f82
falker <maurizio.falconi47@gmail.com>
2025-05-27 13:56:05 +0200

    Add 'Carte senza Umanità' project and related assets; update translations

--- script.js
+++ script.js
@@ -68,7 +68,8 @@ const translations = {
     CracktheCode: "Gioco di logica basato sulle dinamiche di Mastermind e Hit and Blow in cui devi decifrare un codice segreto usando deduzione e feedback progressivi.",
     LedgerLite: "Questa applicazione ti permette di gestire in modo semplice i tuoi crediti e debiti. In ogni operazione viene specificato il nominativo, l’importo e una descrizione.",
     Ripetizioni: "Il sito web dove presento la mia attività di ripetizioni dove i miei studenti possono prenotare le lezioni di matematica e fisica e consultare il mio materiale didattico.",
-    NFT: "White Paper (in italiano) sulla valorizzazione dei dipendenti tramite NFT gamification, adottabile dalle aziende."
+    NFT: "White Paper (in italiano) sulla valorizzazione dei dipendenti tramite NFT gamification, adottabile dalle aziende.",
+    Carte_S_Umanità: "Versione web tutta italiana del gioco di carte 'Cards Against Humanity', dove i giocatori completano frasi con risposte divertenti o provocatorie.",
 
   },
   en: {
@@ -105,7 +106,8 @@ const translations = {
     CracktheCode: "A logic game inspired by the mechanics of Mastermind and Hit and Blow, where you must decipher a secret code using deduction and progressive feedback.",
     LedgerLite: "This application allows you to easily manage your credits and debts. Every transaction includes the name, the amount, and a description.",
     Ripetizioni: "The website where I showcase my tutoring services, where my students can book math and physics lessons and consult my teaching materials.",
-    NFT: "White Paper (in italian) about an employees valoritazion system via NFT gamification, adoptable by companies."
+    NFT: "White Paper (in italian) about an employees valoritazion system via NFT gamification, adoptable by companies.",
+    Carte_S_Umanità: "Italian web version of the card game 'Cards Against Humanity', where players complete sentences with funny or provocative answers."
   }
 };
 
